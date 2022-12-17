5985fc091a4c70870f0e50f82f4baa9b70801284
Janwry Dela Cruz <janwry.jrv@gmail.com>
2022-12-17 15:33:35 +0800

    making of todolist not finish yet

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
+import ToDoList from './ToDoList';
+import {useState} from 'react';
 function App() {
+  const [todos, setTodos] = useState(['Todo 1', 'Todo 2'])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ToDoList />
+      <input type="text"/>
+      
+      <button>Add Todo</button>
+      <button>Clear Complete</button> 
+      <div>0 left to do</div>
+    </>
   );
 }
 
---
+++ to-do-list/src/ToDoList.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Todo from './Todo';
+
+function ToDoList({todos}){
+    return(
+      todos.map
+    )
+}
+export default ToDoList;
